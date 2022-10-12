5f91bc84484694db429fbc25e1e12e36891d3f1f
keemsir <74044742+keemsir@users.noreply.github.com>
2022-10-12 13:28:19 +0900

    Update slack_alarm.py

--- slack_alarm.py
+++ slack_alarm.py
@@ -1,15 +1,16 @@
-
 import slack_sdk
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-slack_token = 'xoxb-' # input token
-client = slack_sdk.WebClient(token=slack_token)
+def slack_msg(token:str, channel_id:str):
 
-try:
-    response = client.chat_postMessage(
-        channel='', # channel id
-        text='Task is completed. :thumbsup:'
-    )
-except SlackApiError as e:
-    assert e.response["error"]
+    slack_token = token # input token
+    client = slack_sdk.WebClient(token=slack_token)
+
+    try:
+        response = client.chat_postMessage(
+            channel=channel_id, # channel id
+            text='Task is completed. :thumbsup:'
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
