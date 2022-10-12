e892125cb309868f662ecb03bbc1d877d9f6df82
keemsir <74044742+keemsir@users.noreply.github.com>
2022-10-12 11:37:05 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+VERSION = "0.0.1"
+
+setup(
+  name='slack_msg',
+  version=VERSION,
+  url='https://github.com/keemsir/salck-msg',
+  author='keemsir',
+  author_email='keemsir@gmail.com',
+  description='for alarm',
+  license='keemsir'
+)
