a570bdc58cb3e80b9f3bd4765e782b346b9e8076
keemsir <74044742+keemsir@users.noreply.github.com>
2022-10-12 13:43:18 +0900

    Update slack_alarm.py

--- slack_alarm.py
+++ slack_alarm.py
@@ -3,13 +3,17 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 def slack_msg(TOKEN:str, CH:str, MSG:str='Task is completed. :thumbsup:'):
+    
+    # TOKEN: input the token
+    # CH: input the channel id
+    # MSG: input the massage, default: 'Task is completed. :thumbsup:'
 
-    slack_token = TOKEN # input token
+    slack_token = TOKEN
     client = slack_sdk.WebClient(token=slack_token)
 
     try:
         response = client.chat_postMessage(
-            channel=CH, # channel id
+            channel=CH,
             text=MSG
         )
     except SlackApiError as e:
