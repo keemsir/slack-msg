bfdbf6b8669d496cdc6dba07476efcf562fe6459
keemsir <74044742+keemsir@users.noreply.github.com>
2023-04-06 11:22:05 +0900

    Update tele_alarm.py

--- util_msg/tele_alarm.py
+++ util_msg/tele_alarm.py
@@ -8,3 +8,14 @@ def tele_msg(TOKEN: str, ID: int, MSG: str='Task is completed. :thumbsup:'):
   
   bot = telegram.Bot(token=TOKEN)
   bot.sendMessage(chat_id=ID, text=MSG)
+
+
+def tele_img(TOKEN: str, ID: int, IMG_PATH: str):
+  
+  # TOKEN: Input the token
+  # ID: Input the chat ID
+  # MSG: Input the message
+  
+  bot = telegram.Bot(token=TOKEN)
+  bot.send_photo(chat_id=ID, photo=open(IMG_PATH, 'rb'))
+  
